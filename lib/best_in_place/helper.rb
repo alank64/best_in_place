66f55d023b9907acad2abaab04c131e0a4097e5f
Andrew Lank <andrew.lank@gmail.com>
2012-08-10 00:06:53 -0400

    Adds ability to update another ID on the fly in the event that it is used for editing. The id must be unique

--- lib/best_in_place/helper.rb
+++ lib/best_in_place/helper.rb
@@ -45,6 +45,7 @@ module BestInPlace
       out << " data-inner-class='#{opts[:inner_class]}'" if opts[:inner_class]
       out << " data-html-attrs='#{opts[:html_attrs].to_json}'" unless opts[:html_attrs].blank?
       out << " data-original-content='#{attribute_escape(real_object.send(field))}'" if opts[:display_as] || opts[:display_with]
+      out << " data-updatable='#{opts[:updatable]}'" unless opts[:updatable].blank?
       if opts[:data] && opts[:data].is_a?(Hash)
         opts[:data].each do |k, v|
           if !v.is_a?(String) && !v.is_a?(Symbol)
